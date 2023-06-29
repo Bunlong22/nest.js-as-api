1d4e2c63520e13d878934c27adad808f1295aa62
bunlong22 <suonbunlong93@gmail.com>
2023-06-29 23:08:19 +0700

    added product controller to controll the model

---
+++ src/products/products.controller.ts
@@ -0,0 +1,50 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+
+
+@Controller('products')
+export class ProductsController {
+  private currentId = 1;
+
+  constructor(private readonly productsService: ProductsService) { }
+
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    const generatedId = this.currentId;
+    this.currentId++;
+    this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+    return { id: generatedId };
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string) {
+    this.productsService.deleteProduct(prodId);
+    return null;
+  }
+}
