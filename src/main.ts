7c4d5695fbc00190d55c498d2f96e60c5720070e
bunlong22 <suonbunlong93@gmail.com>
2023-06-29 23:06:49 +0700

    enable cors and add the custom port number

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.use(cors({ origin: 'http://localhost:4500' }));
+
   await app.listen(3000);
 }
 bootstrap();
+
